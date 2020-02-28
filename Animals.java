aeb543ea793b6b21c863ef8d4771eb93b54aa535
hussamAdil <hussam0683@gmail.com>
2020-02-28 20:47:16 +0200

    set git

---
+++ Animals.java
@@ -0,0 +1,23 @@
+
+public class Animals {
+
+	
+ public Animals(String name, int age, String color) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.color = color;
+	}
+ 
+String name ;
+
+ int age ;
+ 
+ String color;
+ 
+ public void showData()
+ {
+	 System.out.print(name +' ' + age + ' ' + color);
+ }
+ 
+}
---
+++ Zoo.java
@@ -0,0 +1,14 @@
+
+public class Zoo {
+
+	public static void main(String[] args) {
+		 
+		Animals cat = new Animals("cat", 2, "black");
+		cat.showData();
+		Animals leno = new Animals("leno", 5, "red");
+		leno.showData();
+		Animals dog = new Animals("dog", 2, "green");
+		dog.showData();
+	}
+
+}
