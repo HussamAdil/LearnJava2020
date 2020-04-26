839479a5a5d325c48cd19971c66aac4cd1a3cc2b
hussamAdil <hussam0683@gmail.com>
2020-04-26 13:09:07 +0200

    loop gift problem

---
+++ Loop.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+
+public class Loop {
+    public static void main(String[] args) {
+          int  gift = 10 ;
+          int boys = 5 ;
+        for(int n = 0 ; gift > 0 ; n+=2)
+        {
+            System.out.println("Now You have = " + gift);
+            gift = gift - 2 ;
+        }
+     }
+}
