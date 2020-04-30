3c4354b13c84e99c37cb9e06b85df935645c16fb
hussamAdil <hussam0683@gmail.com>
2020-04-30 21:31:12 +0200

    problem Longest String

---
+++ FindLongestString.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+
+public class FindLongestString {
+    public static void main(String[] args) {
+         /*
+        Find the index for the longest String in array
+     */
+
+        //data set
+        String employees[] = {"omer", "Hussam", "ali"};
+
+        for (int i = 0; i <= employees.length - 1; i++) {
+          }
+
+    }
+}
