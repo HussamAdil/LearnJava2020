4ec7bc93f50cd6ddf6b9103ca6df6ff98606949b
hussamAdil <hussam0683@gmail.com>
2020-05-02 21:57:44 +0200

    Learn interface

---
+++ Shop.java
@@ -0,0 +1,18 @@
+public class Shop  {
+
+
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop() ;
+        laptop.pay();
+     }
+}
+class Laptop implements Pay {
+    int price = 1000 ;
+    int deliveryCost = 5;
+
+    @Override
+    public  void pay() {
+        System.out.println( price + deliveryCost);
+    }
+
+}
